887c11ef6b66d72eba3f3e731bf176af673f6ff4
gginestra <guillermogidi@msn.com>
2014-11-12 12:17:19 +0100

    Servicio SOAP implementado

---
+++ TodoSOAP-server/src/main/java/bigws/todo/domain/ToDoItem.java
@@ -0,0 +1,53 @@
+package bigws.todo.domain;
+
+public class ToDoItem {
+
+	public String task;
+	public String context;
+	public String project;
+	public String priority;
+	
+	public ToDoItem(String t, String c, String pro, String pri){
+		task = t;
+		context = c;
+		project = pro;
+		priority = pri; 
+		
+	}
+	
+	public ToDoItem() {
+	}
+
+	public String getTask() {
+		return task;
+	}
+	
+	public void setTask(String task) {
+		this.task = task;
+	}
+	
+	public String getContext() {
+		return context;
+	}
+	
+	public void setContext(String context) {
+		this.context = context;
+	}
+	
+	public String getProject() {
+		return project;
+	}
+	
+	public void setProject(String project) {
+		this.project = project;
+	}
+	
+	public String getPriority() {
+		return priority;
+	}
+	
+	public void setPriority(String priority) {
+		this.priority = priority;
+	}
+	
+}
---
+++ TodoSOAP-server/src/main/java/bigws/todo/service/TodoWebService.java
@@ -0,0 +1,134 @@
+package bigws.todo.service;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
+import javax.jws.WebMethod;
+import javax.jws.WebService;
+
+import com.google.gson.Gson;
+
+import bigws.todo.domain.*;
+
+@WebService
+public class TodoWebService {
+	public final static String DEFAULT_FILE_NAME = "ToDo.json";
+	
+	@WebMethod()
+	public ToDoItem addTodo(String task, String pro, String cntx, String pri) {
+		
+		ToDoItem item = new ToDoItem(task,pro,cntx,pri);
+		
+		ToDoList toDoList = new ToDoList();
+		Gson gson = new Gson();
+		try {
+			toDoList = gson.fromJson(new FileReader(DEFAULT_FILE_NAME), ToDoList.class);
+		} catch (FileNotFoundException e) {
+			System.out.println("**File Not Found");
+			File file = new File(DEFAULT_FILE_NAME);
+		}
+		
+		try {
+			toDoList.addItem(item);
+			FileWriter output = new FileWriter(DEFAULT_FILE_NAME);
+			output.write(gson.toJson(toDoList));
+			output.close();
+		} catch (IOException e) {
+			System.out.println("**Error written in file");
+		}
+		
+		return item;
+	}
+	
+	@WebMethod()
+	public String removeTodo(String by, String keyword) {
+		ToDoList toDoList = new ToDoList();
+		ToDoList toDoList_final = new ToDoList();
+		Gson gson = new Gson();
+		try {
+			toDoList = gson.fromJson(new FileReader(DEFAULT_FILE_NAME), ToDoList.class);
+			File file = new File(DEFAULT_FILE_NAME);
+			if(file.length()!=0){
+				for (ToDoItem item : toDoList.getToDoList()) {
+					
+					if(by.equals("task")){
+						if(!item.getTask().contains(keyword)){
+							toDoList_final.addItem(item);
+						}							
+					}else if(by.equals("project")){
+						if(!item.project.contains(keyword)){
+							toDoList_final.addItem(item);
+						} 
+					}else if(by.equals("context")){
+						if(!item.context.contains(keyword)){
+							toDoList_final.addItem(item);
+						} 
+					}else if(by.equals("priority")){
+						if(!item.priority.contains(keyword)){
+							toDoList_final.addItem(item);
+						} 
+					}
+					
+				}
+			}else{
+				System.out.println("**Empty File");
+			}
+			
+			FileWriter output = new FileWriter(DEFAULT_FILE_NAME);
+			output.write(gson.toJson(toDoList_final));
+			output.close();			
+			
+		} catch (FileNotFoundException e) {
+			System.out.println("**File Not Found");
+		} catch (IOException e) {
+			System.out.println("**File Not Found");
+		}
+			
+		return "Todo whith " + by + "was " + keyword + "has been deleted";
+	}
+	
+	@WebMethod()
+	public ToDoList listTodo(String by, String keyword) {
+		ToDoList toDoList = new ToDoList();
+		ToDoList toDoList_response = new ToDoList();
+		Gson gson = new Gson();
+		try {
+			toDoList = gson.fromJson(new FileReader(DEFAULT_FILE_NAME), ToDoList.class);
+			File file = new File(DEFAULT_FILE_NAME);
+			if (file.length() != 0){
+				for (ToDoItem item : toDoList.getToDoList()) {
+					
+					if(by.equals("task")){
+						if(!item.getTask().contains(keyword)){
+							toDoList_response.addItem(item);
+						}							
+					}else if(by.equals("project")){
+						if(!item.project.contains(keyword)){
+							toDoList_response.addItem(item);
+						} 
+					}else if(by.equals("context")){
+						if(!item.context.contains(keyword)){
+							toDoList_response.addItem(item);
+						} 
+					}else if(by.equals("priority")){
+						if(!item.priority.contains(keyword)){
+							toDoList_response.addItem(item);
+						} 
+					}
+				}
+				
+			}else{
+				System.out.println("**File Empty");
+			}
+			
+		}catch (FileNotFoundException e) {
+			System.out.println("**File Not Found");			
+			
+		}
+		
+		return toDoList_response;
+	}
+}
