3fc2a7f44794a4a494c0d206c598583ee715110a
gginestra <guillermogidi@msn.com>
2014-11-26 20:06:43 +0100

    metodo add

--- TodoSOAP-cli-ws/src/main/java/bigws/todo/servlets/Form.java
+++ TodoSOAP-cli-ws/src/main/java/bigws/todo/servlets/Form.java
@@ -12,27 +12,48 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 
+import javax.xml.ws.WebServiceRef;
+
+import bigws.todo.service.*;
+
 import com.google.gson.Gson;
 
 @SuppressWarnings("serial")
-@WebServlet(urlPatterns = { "/add" })
+@WebServlet(name = "Form", urlPatterns = { "/add","/remove","/list" })
 
-public class FormularioAdd extends HttpServlet  {
+public class Form extends HttpServlet  {
 
-	public final static String DEFAULT_FILE_NAME = "ToDo.json";
+	@WebServiceRef(wsdlLocation =  "WEB-INF/wsdl/ToDoWsCliService.wsdl")
+	private TodoWebService service;
 	
 	
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp)
 			throws ServletException, IOException {
 		
-		String task = req.getParameter("task");
-		String pro = req.getParameter("project");
-		String cntx = req.getParameter("context");
-		String pri = req.getParameter("priority");
+		TodoWebServiceService todowss = new TodoWebServiceService();
+		TodoWebService todows = todowss.getTodoWebServicePort();
+		ToDoList toDoList = new ToDoList();
+		
+		String res = ""; 
+		String URI = req.getRequestURI();
+		
+		if (URI.contains("add")){
+			String task = req.getParameter("task");
+			String pro = req.getParameter("project");
+			String cntx = req.getParameter("context");
+			String pri = req.getParameter("priority");
+			todows.addTodo(task, pro, cntx, pri);
+			
+		}else if (URI.contains("remove")){
+			
+			
+			
+		}else if (URI.contains("list")){
+			
+		}
 			
 		
 	//	ToDoList toDoList = new ToDoList();
-		Gson gson = new Gson();
 
 		
 //		toDoList = gson.fromJson(new FileReader(DEFAULT_FILE_NAME), ToDoList.class);
